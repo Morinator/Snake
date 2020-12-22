77f7bd95ad47fd2aa2134d17c0c2312f454821f2
Moritz Gross <mgross321@gmail.com>
2020-12-22 03:50:26 +0100

    reformatting

--- configs/game_constants.py
+++ configs/game_constants.py
@@ -1,8 +1,4 @@
 import pygame as pg
 
-up = 0, -1
-down = 0, 1
-left = -1, 0
-right = 1, 0
-
+up, down, left, right = (0, -1), (0, 1), (-1, 0), (1, 0)
 input_movements = {pg.K_LEFT: left, pg.K_RIGHT: right, pg.K_DOWN: down, pg.K_UP: up}
