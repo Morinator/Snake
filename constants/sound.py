a86f229612e23c2a0214b5ebd95a33c37b89988c
Moritz Gross <mgross321@gmail.com>
2020-12-24 00:55:02 +0100

    uses os module for paths to sounds now

--- constants/sound.py
+++ constants/sound.py
@@ -1,10 +1,14 @@
+import os
+
 import pygame as pg
 
+sound_root = os.path.join(os.path.dirname(__file__), '..', 'assets', 'sounds')
+
 # music
-pg.mixer.music.load("assets/sounds/mario.mp3")
+pg.mixer.music.load(os.path.join(sound_root, "mario.mp3"))
 pg.mixer.music.set_volume(0.75)
 pg.mixer.music.play(-1)  # on loop
 
 # sounds
-MUNCH_SOUND = pg.mixer.Sound("assets/sounds/munch.wav")
-TELEPORTATION_SOUND = pg.mixer.Sound("assets/sounds/teleport.wav")
+MUNCH_SOUND = pg.mixer.Sound(os.path.join(sound_root, "munch.wav"))
+TELEPORTATION_SOUND = pg.mixer.Sound(os.path.join(sound_root, "teleport.wav"))
