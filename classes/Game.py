0d5540ce77bf046e132dfd3e67bf89d1023a07ff
Moritz Gross <mgross321@gmail.com>
2020-12-23 19:47:17 +0100

    uses Game-class now

--- classes/Game.py
+++ classes/Game.py
@@ -1,21 +1,51 @@
+import sys
+
 import pygame as pg
 
-from configs.settings import SCREEN_SIZE
+from classes.Apple import Apple
+from classes.Grid import Grid
+from classes.Snake import Snake
+from configs.colors import LIGHT_GREY
+from configs.settings import SCREEN_SIZE, GRID_SIZE, MAX_FPS, FONT, TEXT_POS
 
 
 class Game:
     def __init__(self):
+        self.score = 0
+        self.grid = Grid(*GRID_SIZE)
+        self.snake = Snake(self.grid)
+        self.apple = Apple(self.grid)
         self.screen = pg.display.set_mode(SCREEN_SIZE)
         self.clock = pg.time.Clock()
 
-    def run(self):
-        pass
+    def draw(self, screen):
+        [item.draw(screen) for item in [self.grid, self.apple, self.snake]]
+        screen.blit(FONT.render(f"Score: {self.score}", True, LIGHT_GREY), TEXT_POS)
+        pg.display.update()
 
-    def draw(self):
-        pass
+    def run(self):
+        while True:
+            self.clock.tick(MAX_FPS)
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    sys.exit()
+                if event.type == pg.KEYDOWN:
+                    if event.key == pg.K_ESCAPE:
+                        sys.exit()
+                    self.snake.update_direction(event.key)
+
+            has_collided = self.snake.move(self.grid)
+            self.snake.update_color()
+            if has_collided:
+                sys.exit()
+
+            if self.snake.li[0] == self.apple.pos:
+                self.snake.eat()
+                self.apple = Apple(self.grid)
+                self.score += 1
+
+            self.draw(self.screen)
 
     def show_start_screen(self):
-        pass
-
-    def new_game(self):
         pass
--- main.py
+++ main.py
@@ -1,46 +1,7 @@
 import pygame as pg
 
 pg.init()
-from classes.Apple import Apple
-from classes.Grid import Grid
-from classes.Snake import Snake
-from configs.colors import *
-from configs.settings import *
-import sys
+from classes.Game import Game
 
-
-def update_all_graphics():
-    [item.draw(screen) for item in [grid, apple, snake]]
-    screen.blit(FONT.render(f"Score: {score}", True, LIGHT_GREY), TEXT_POS)
-    pg.display.update()
-
-
-screen = pg.display.set_mode(SCREEN_SIZE)
-clock = pg.time.Clock()
-score = 0
-
-grid = Grid(*GRID_SIZE)
-apple = Apple(grid)
-snake = Snake(grid)
-while True:
-    clock.tick(MAX_FPS)
-
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            sys.exit()
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_ESCAPE:
-                sys.exit()
-            snake.update_direction(event.key)
-
-    has_collided = snake.move(grid)
-    snake.update_color()
-    if has_collided:
-        sys.exit()
-
-    if snake.li[0] == apple.pos:
-        snake.eat()
-        apple = Apple(grid)
-        score += 1
-
-    update_all_graphics()
+g = Game()
+g.run()
