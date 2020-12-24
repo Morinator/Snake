94decd27e90c507d7ea06e14e75c4e76fcead4e5
Moritz Gross <mgross321@gmail.com>
2020-12-24 15:40:25 +0100

    removed music

--- constants/sound.py
+++ constants/sound.py
@@ -1,14 +1,9 @@
-import os
+from os import path
 
 import pygame as pg
 
-sound_root = os.path.join(os.path.dirname(__file__), '..', 'assets', 'sounds')
+sound_root = path.join(path.dirname(__file__), '..', 'assets', 'sounds')
 
-# music
-pg.mixer.music.load(os.path.join(sound_root, "mario.mp3"))
-pg.mixer.music.set_volume(0.75)
-pg.mixer.music.play(-1)  # on loop
-
-# sounds
-MUNCH_SOUND = pg.mixer.Sound(os.path.join(sound_root, "munch.wav"))
-TELEPORTATION_SOUND = pg.mixer.Sound(os.path.join(sound_root, "teleport.wav"))
+# sound effects
+MUNCH_SOUND = pg.mixer.Sound(path.join(sound_root, "munch.wav"))
+TELEPORTATION_SOUND = pg.mixer.Sound(path.join(sound_root, "teleport.wav"))
