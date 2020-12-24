d630460c82b5d3d87a4960782cf428b298927c53
Moritz Gross <mgross321@gmail.com>
2020-12-24 17:57:18 +0100

    new font, which is read in with match_font

--- constants/settings.py
+++ constants/settings.py
@@ -10,6 +10,7 @@ TILE_SIZE = 40
 SCREEN_SIZE = TILE_SIZE * GRID_SIZE[0], TILE_SIZE * GRID_SIZE[1]
 APPLE_SIZE = -(TILE_SIZE * 0.2)
 SNAKE_SIZE = -(TILE_SIZE * 0.1)
+
 # text
 TEXT_POS = 10, 10
-FONT = pg.font.Font('freesansbold.ttf', int(0.08 * SCREEN_SIZE[0]))
+FONT = pg.font.Font(pg.font.match_font('arial'), int(0.08 * SCREEN_SIZE[0]))
